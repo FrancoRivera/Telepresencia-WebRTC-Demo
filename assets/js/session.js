8e44132331d01d3ce98aa0c513c0cc2087ef95a6
Franco Rivera <franco@riverarivas.com>
2018-11-06 08:17:08 -0500

    adding downloading example and NetworkInfo (chrome only)

--- assets/js/session.js
+++ assets/js/session.js
@@ -38,6 +38,50 @@ var clientMessages={
 
 var inRoom = {}
 
+window.setInterval(function(){
+  // check downlink for mozilla
+  console.log("Network Information: " + navigator.connection.downlink + "Mb/s")
+  console.log('rtt (round trip): ' + navigator.connection.rtt + 'ms');
+  console.log('effectiveType: ' + navigator.connection.effectiveType);
+
+}, 1000)
+function downloadSampleFile(){
+  // Taken From
+  // https://gist.github.com/debloper/7296289
+  // Let's initialize the primitives
+  var startTime, endTime, fileSize;
+
+  // Set up the AJAX to perform
+  var xhr = new XMLHttpRequest();
+
+  // Rig the call-back... THE important part
+  xhr.onreadystatechange = function () {
+
+    // we only need to know when the request has completed
+    if (xhr.readyState === 4 && xhr.status === 200) {
+
+      // Here we stop the timer & register end time
+      endTime = (new Date()).getTime();
+
+      // Also, calculate the file-size which has transferred
+      fileSize = xhr.responseText.length;
+
+      // Calculate the connection-speed
+      var speed = (fileSize * 8) / ((endTime - startTime)/1000) / 1024;
+
+      // Report the result, or have fries with it...
+      console.log(speed + " Kbps\n");
+    }
+  }
+
+  // Snap back; here's where we start the timer
+  startTime = (new Date()).getTime();
+
+  // All set, let's hit it!
+  xhr.open("GET", "URL/TO/PROBE.FILE", true);
+  xhr.send();
+
+}
 //Get room key from server
 function setClientMessage(key){
   $("#roomStatus").html(clientMessages[key])
